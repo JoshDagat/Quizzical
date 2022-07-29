2dad772016b64a15fdcb0a1186caf90f421ec346
JosuerBague <josuer.online.work@gmail.com>
2022-07-29 15:32:30 +0800

    Modified App to include an Intro component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Quiz from "./components/Quiz/Quiz";
+import Intro from "./components/Intro/Intro";
 import "./App.css";
 
 
 export default function App() {
-
+    const [init, setInit] = React.useState(true);
     return (
         <div className="App">
-            <Quiz />
+            {init ? <Intro /> : <Quiz />}
         </div>
     )
 }
